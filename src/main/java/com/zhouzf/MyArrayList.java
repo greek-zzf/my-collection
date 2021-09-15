65746761194ff5852444a45c0231d34e4af64322
Zhouzf <aaaa159557190@gmail.com>
2021-09-15 14:22:53 +0800

    Initialize commit

---
+++ src/main/java/com/zhouzf/MyArrayList.java
@@ -0,0 +1,8 @@
+package com.zhouzf;
+
+/**
+ * @author Zhaofeng Zhou
+ * @date 2021/9/15/015 14:18
+ */
+public class MyArrayList {
+}
---
+++ src/test/java/com/zhouzf/MyArrayListTest.java
@@ -0,0 +1,15 @@
+package com.zhouzf;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author Zhaofeng Zhou
+ * @date 2021/9/15/015 14:19
+ */
+public class MyArrayListTest {
+
+    @Test
+    void smokeTest() {
+        System.out.println("冒烟测试");
+    }
+}
